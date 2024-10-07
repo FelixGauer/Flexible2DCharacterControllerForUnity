6778381899d4a3b223c9c5445cc206133e99039d
Vladislav-EG <vlad.egipko@mail.ru>
2024-10-07 11:05:02 +0300

    Added NewInputSystem
    1. Added newInputSystem;
    2. Created and configured Actions;
    3. Created inputReader script

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static PlayerInputActions;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Input/InputReader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+	public event UnityAction<Vector2> Move = delegate { };	
+	public event UnityAction<bool> Jump = delegate { };	
+	
+	private PlayerInputActions _inputActions;
+
+	public Vector3 Direction => _inputActions.Player.Move.ReadValue<Vector2>();
+	
+	private void OnEnable()
+	{
+		Debug.Log("EnableInputReaderScript");
+		if (_inputActions == null)
+		{
+			_inputActions = new PlayerInputActions();
+			_inputActions.Player.SetCallbacks(this);
+			
+		}
+		_inputActions.Enable();
+	}
+	
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		Move.Invoke(context.ReadValue<Vector2>());
+		Debug.Log($"{context.ReadValue<Vector2>()} + Move");
+	}
+	
+	public void OnJump(InputAction.CallbackContext context)
+	{		
+		// if (context.phase == InputActionPhase.Performed)
+		// {
+		// 	Debug.Log($"{context.phase} + Jump");
+		// 	Jump.Invoke();
+		// }
+		switch (context.phase) 
+		{
+				case InputActionPhase.Started:
+					Jump.Invoke(true);
+					break;
+				case InputActionPhase.Canceled:
+					Jump.Invoke(false);
+					break;
+		}
+		
+		Debug.Log($"{context.phase} + Jump");
+	}
+}
---
+++ Assets/Scripts/InputReaderTest.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReaderTest : MonoBehaviour
+{
+    [SerializeField] private InputReader inputReader;
+}
