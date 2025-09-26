a94ca6b9716823aed478f964ff4d81eedecc0793
FelixGauer <154312503+FelixGauer@users.noreply.github.com>
2025-09-26 17:27:53 +0200

    I have the power of a GOOOOD
    Implemented simple text progression

--- Assets/DatingSim_Controller.cs
+++ Assets/DatingSim_Controller.cs
@@ -1,16 +1,52 @@
+using TMPro;
 using UnityEngine;
 
 public class DatingSim_Controller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject textbox;
+    public GameObject background;
+    public GameObject charImg;
+    public TMP_Text dialogText;
+    public string[] text_string;
+
+    int currentDialogID;
+
+
     void Start()
     {
-        
+        //Set initial data? neccessary?
+            
+        SetDSActive();
+        currentDialogID = 0;
+
+    }
+
+    void ContinueDialoge ()
+    {
+        currentDialogID++;
+        dialogText.text = text_string[currentDialogID];
+
+    }
+
+    void SetDSActive()
+    {
+        background.SetActive(true);
+        charImg.SetActive(true);
+        textbox.SetActive(true);
+        dialogText.text = text_string[currentDialogID];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        ContinueDialoge();
+
+        }
+
+
     }
 }
