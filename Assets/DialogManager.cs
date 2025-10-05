bbfa676e37c8d53485a986928cffa4f32ba13f38
Kinshuk <kinshuk364@gmail.com>
2025-10-05 20:39:55 +0200

    Now the array in node is used before jumping to another one

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 using XNode;
 
 public class DialogManager : MonoBehaviour
-
 {
     // DialogAsset does not handle GameObject textbox, background or charImg, just contains the data to call it.
 
     public GameObject dsUI;
-    
+
     public TMP_Text dialogText;
     public NodeGraph dialogGraph;
     public DialogNode currentNode;
-
+    public int elementIndex = 0;
     private bool isDSactive = true;
 
     int currentDialogIndex = 0;
@@ -22,23 +21,30 @@ public class DialogManager : MonoBehaviour
         // set active UI elements (). This function would be called from the JnR level again.
         currentNode = dialogGraph.nodes[0] as DialogNode;
         ShowCurrentline();
-
     }
 
     void ShowCurrentline()
     {
-        if (currentNode != null)
-            dialogText.text = currentNode.dialogText[0];
+        if (currentNode != null && elementIndex < currentNode.dialogText.Length)
+        {
+            dialogText.text = currentNode.dialogText[elementIndex];
+            elementIndex++;
+        }
+        else
+        {
+            ContinueDialoge();
+            elementIndex = 0;
+            ShowCurrentline();
+        }
     }
 
     void ContinueDialoge()
     {
-
         // change so:
         // Instead of 1 String, go through all Strings in the array, THEN go to the next node
         if (isDSactive)
         {
-
+            Debug.Log(" Continue Dialoge ");
             NodePort port = currentNode.GetOutputPort("nextNodes 0");
             if (port == null || port.ConnectionCount == 0)
                 return;
@@ -46,29 +52,26 @@ public class DialogManager : MonoBehaviour
             currentNode = port.GetConnection(0).node as DialogNode;
             ShowCurrentline();
         }
+        
     }
 
-    void SwitchGame() 
+    void SwitchGame()
     {
         isDSactive = !isDSactive;
-        dsUI .SetActive(isDSactive);
-
-
+        dsUI.SetActive(isDSactive);
     }
 
-        void Update()
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
+            // ContinueDialoge();
+            ShowCurrentline();
+        }
 
-            if (Input.GetMouseButtonDown(0))
-            {
-                ContinueDialoge();
-            }
-
-            if (Input.GetKeyDown(KeyCode.K)) 
-            {
-                SwitchGame();
-            }
-
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SwitchGame();
         }
     }
-
+}
