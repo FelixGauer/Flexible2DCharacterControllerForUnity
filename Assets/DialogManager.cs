dad37bafa7d4a9ab72ff3650eabd5d91d1a75896
FelixGauer <154312503+FelixGauer@users.noreply.github.com>
2025-10-05 14:45:45 +0200

    Added switching screens

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -7,9 +7,13 @@ public class DialogManager : MonoBehaviour
 {
     // DialogAsset does not handle GameObject textbox, background or charImg, just contains the data to call it.
 
+    public GameObject dsUI;
+    
     public TMP_Text dialogText;
     public NodeGraph dialogGraph;
     public DialogNode currentNode;
+
+    private bool isDSactive = true;
 
     int currentDialogIndex = 0;
 
@@ -32,13 +36,24 @@ public class DialogManager : MonoBehaviour
 
         // change so:
         // Instead of 1 String, go through all Strings in the array, THEN go to the next node
+        if (isDSactive)
+        {
 
-        NodePort port = currentNode.GetOutputPort("nextNodes 0");
-        if (port == null || port.ConnectionCount == 0)
-            return;
+            NodePort port = currentNode.GetOutputPort("nextNodes 0");
+            if (port == null || port.ConnectionCount == 0)
+                return;
 
-        currentNode = port.GetConnection(0).node as DialogNode;
-        ShowCurrentline();
+            currentNode = port.GetConnection(0).node as DialogNode;
+            ShowCurrentline();
+        }
+    }
+
+    void SwitchGame() 
+    {
+        isDSactive = !isDSactive;
+        dsUI .SetActive(isDSactive);
+
+
     }
 
         void Update()
@@ -47,6 +62,11 @@ public class DialogManager : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 ContinueDialoge();
+            }
+
+            if (Input.GetKeyDown(KeyCode.K)) 
+            {
+                SwitchGame();
             }
 
         }
