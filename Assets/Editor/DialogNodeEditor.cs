c3a9c6eb7aa16aa1ad5906f6ed73377451637617
FelixGauer <154312503+FelixGauer@users.noreply.github.com>
2025-11-03 17:57:18 +0100

    Added DialogNodeEditor for better UI readability

---
+++ Assets/Editor/DialogNodeEditor.cs
@@ -0,0 +1,87 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using XNode;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(DialogNode))]
+public class DialogNodeEditor_Custom : NodeEditor
+{
+    public override void OnBodyGUI()
+    {
+        serializedObject.Update();
+
+        // --- Draw your normal fields (unchanged) ---
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("nodeID"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("teleportLocation"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("whostalking"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("character_img"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("background_img"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("nodeQuestion"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("questionTexts"));
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("questionObjects"));
+
+        // --- Input port ---
+        NodeEditorGUILayout.PortField(target.GetInputPort("inputNode"));
+
+        // --- Custom drawing for dialogText[] as large text areas ---
+        var linesProp = serializedObject.FindProperty("dialogText");
+        EditorGUILayout.Space(6);
+        EditorGUILayout.LabelField("Dialog Lines", EditorStyles.boldLabel);
+
+        if (linesProp != null && linesProp.isArray)
+        {
+            for (int i = 0; i < linesProp.arraySize; i++)
+            {
+                var elem = linesProp.GetArrayElementAtIndex(i);
+
+                EditorGUILayout.BeginVertical("box");
+                EditorGUILayout.LabelField($"Line {i + 1}", EditorStyles.miniBoldLabel);
+
+                // Large TextArea for each line
+                elem.stringValue = EditorGUILayout.TextArea(elem.stringValue, GUILayout.MinHeight(80));
+
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Insert Above"))
+                {
+                    linesProp.InsertArrayElementAtIndex(i);
+                    linesProp.GetArrayElementAtIndex(i).stringValue = "";
+                    break; // stop drawing this frame to avoid index issues
+                }
+                if (GUILayout.Button("Remove"))
+                {
+                    linesProp.DeleteArrayElementAtIndex(i);
+                    break;
+                }
+                if (GUILayout.Button("Add Below"))
+                {
+                    linesProp.InsertArrayElementAtIndex(i + 1);
+                    linesProp.GetArrayElementAtIndex(i + 1).stringValue = "";
+                    break;
+                }
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.EndVertical();
+            }
+
+            if (GUILayout.Button("Add Line"))
+            {
+                int idx = linesProp.arraySize;
+                linesProp.InsertArrayElementAtIndex(idx);
+                linesProp.GetArrayElementAtIndex(idx).stringValue = "";
+            }
+        }
+
+        // --- Output ports (dynamic list) ---
+        NodeEditorGUILayout.DynamicPortList(
+            "nextNodes",
+            typeof(DialogNode),
+            serializedObject,
+            NodePort.IO.Output,
+            Node.ConnectionType.Multiple
+        );
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
